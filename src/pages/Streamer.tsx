62175a393375b85cd3516855605bb0e5d25cc32c
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-06 18:19:30 -0300

    Added new pages to router

---
+++ src/pages/Streamer.tsx
@@ -0,0 +1,5 @@
+export default function Streamer() {
+    return (
+        <h2>Streamer page</h2>
+    )
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from '../pages/Home';
+import StreamerPage from '../pages/Streamer';
+import ContactPage from '../pages/Contact';
 
 const router = createBrowserRouter([
     {
         path: "/",
         Component: HomePage,
-    }
+    },
+    {
+        path: "/contact",
+        Component: ContactPage,
+    },
+    {
+        path: "/streamer/:id",
+        Component: StreamerPage,
+    },
 ]);
 
 export default router;
