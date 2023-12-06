1976522fe5781181370b9fdf0eaf16f34393875b
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-06 20:32:47 -0300

    Right link on the logo

--- src/components/Navbar/Logo.tsx
+++ src/components/Navbar/Logo.tsx
@@ -1,8 +1,10 @@
+import { NavLink } from "react-router-dom";
+
 export default function Logo() {
     return (
-        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
+        </NavLink>
     )
 }
