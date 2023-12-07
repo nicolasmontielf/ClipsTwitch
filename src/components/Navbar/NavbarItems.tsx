51228014dc6bc4e6e845a6dff16a04cda561fe78
Nicolas Montiel <nicolasmontielf@gmail.com>
2023-12-07 19:16:10 -0300

    Added new pages

--- src/components/Navbar/NavbarItems.tsx
+++ src/components/Navbar/NavbarItems.tsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 export default function NavbarItems() {
     const items = [
         { url: '/', label: 'Home' },
+        { url: '/about', label: 'About' },
         { url: '/contact', label: 'Contact' },
-        { url: '/streamer/illojuan', label: 'Streamer' },
     ];
 
     return (
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from '../pages/Home';
 import StreamerPage from '../pages/Streamer';
 import ContactPage from '../pages/Contact';
+import AboutPage from '../pages/About';
 import App from '../App'
 
 const router = createBrowserRouter([
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
             {
                 path: "/streamer/:streamerLogin",
                 element: <StreamerPage />,
+            },
+            {
+                path: "/about",
+                element: <AboutPage />,
             },
         ]
     },
